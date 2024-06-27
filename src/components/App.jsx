1b65f7c650d7872bfb4f1e2c8ccdc6bd7a82cbc7
Louis-Severino-Romano <louis.romano@ctu.edu.ph>
2024-06-27 21:06:27 +0800

    init4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,87 +2,94 @@ import React, { useState, useEffect, useCallback } from 'react';
 
 import { nanoid } from 'nanoid';
 import { ContactList } from './contactList';
-import  ContactForm  from './ContactForm';
+import ContactForm from './ContactForm';
 import { Filter } from './Filter';
 import './App.css';
-
-
-
+import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
-
-  const [contacts, setContacts]=useState([
+  const [contacts, setContacts] = useState([
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    
   ]);
 
-  const [filter,setFilter]=useState('');
+  const [filter, setFilter] = useState('');
 
-useEffect(()=>{
-  const storedContacts=localStorage.getItem('contacts');
-  if (storedContacts){
-    setContacts(JSON.parse(storedContacts));
-  }
-},[]);
+  useEffect(() => {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      setContacts(JSON.parse(storedContacts));
+    }
+  }, []);
 
-useEffect(()=>{
-  localStorage.setItem('contacts',JSON.stringify(contacts));
-},[contacts]);
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-const addContact=useCallback((name,number)=>{
-  const normalizedName=name.trim().toLowerCase();
-  const isDuplicate=contacts.some(contact=> contact.name.toLowerCase()===normalizedName);
+  const addContact = useCallback(
+    (name, number) => {
+      const normalizedName = name.trim().toLowerCase();
 
-  if (isDuplicate){
-    alert (`${name} is already in the phonebook.`);
-  }
+      const isDuplicate = contacts.some(
+        contact => contact.name.toLowerCase() === normalizedName
+      );
 
-  const newContact={
-    id:nanoid,
-    name:name.trim(),
-    number:number.trim()
-  }
+      if (isDuplicate) {
+        toast.error(`${name} is already in the phonebook.`);
+        return
+      }
 
-  setContacts(prevContacts=>[...prevContacts,newContact]);
+      const newContact = {
+        id: nanoid,
+        name: name.trim(),
+        number: number.trim(),
+      };
 
-},[contacts])
-
-const handleFilterChange=(event)=>{
-setFilter(event.target.value);
-}
-
-const deleteContact = (id) => {
-  setContacts(prevContacts => prevContacts.filter(contact => contact.id !== id));
-};
-
-const getFilteredContacts = () => {
-  const normalizedFilter = filter.toLowerCase();
-  return contacts.filter(contact =>
-    contact.name.toLowerCase().includes(normalizedFilter)
+      setContacts(prevContacts => [...prevContacts, newContact]);
+    },
+    [contacts]
   );
-};
 
-const filteredContacts = getFilteredContacts();
+  const handleFilterChange = event => {
+    setFilter(event.target.value);
+  };
 
+  const deleteContact = id => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
+    );
+  };
+
+  const getFilteredContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const filteredContacts = getFilteredContacts();
 
   return (
     <div className="App">
       <div className="container">
         <h2>Phonebook</h2>
+        <Toaster position="top-center" reverseOrder={false} />
         <ContactForm onAddContact={addContact} />
-        
+
         <h2>Contacts</h2>
         <Filter filter={filter} onFilterChange={handleFilterChange} />
-        <ContactList contacts={filteredContacts} onDeleteContact={deleteContact} />
+        <ContactList
+          contacts={filteredContacts}
+          onDeleteContact={deleteContact}
+        />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
 // export class App extends Component {
 //   state = {
@@ -123,7 +130,6 @@ export default App
 //       name: name.trim(),
 //       number: number.trim()
 //     };
-    
 
 //     this.setState(prevState => ({
 //       contacts: [...prevState.contacts, newContact]
@@ -149,7 +155,7 @@ export default App
 //   };
 
 //   render() {
-    
+
 //     const filteredContacts = this.getFilteredContacts();
 //     return (
 //       <div className="App">
@@ -165,4 +171,3 @@ export default App
 //     );
 //   }
 // }
-
